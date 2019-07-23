484cbe1c04d2e0d59279f493adf6ee27863ba68e
constuelo <ferguson.rory@gmail.com>
2019-07-23 09:52:44 +0100

    General Update

--- helpers.py
+++ helpers.py
@@ -16,10 +16,10 @@ def psd_name(path: str, message: str):
                 return file
 
 
-def input_directory(message):
+def valid_directory(message: str) -> str:
     while True:
         try:
-            path = input(f'{message}\n')
+            path = f'{message}\n'
         except ValueError:
             print('Invalid path, try again.')
             continue
@@ -39,3 +39,29 @@ def directory_exists(path: str) -> bool:
     except TypeError:
         pass
 
+
+def decimal_count(count: int) -> str:
+    if count <= 9:
+        return f'0{count}'
+    else:
+        return f'{count}'
+
+
+def underscore(pattern: str) -> str:
+    if not pattern:
+        return pattern
+    elif '_' in pattern and len(pattern) > 1:
+        return ''
+    elif '_' in pattern[-1:]:
+        return pattern
+    elif '_' not in pattern[-1:]:
+        return f'{pattern}_'
+
+
+def convert_width_if_email(width: str) -> str:
+    if width == '850':
+        return 'Mobile'
+    elif width == '1280':
+        return 'Desktop'
+    else:
+        return
--- image_export.py
+++ image_export.py
@@ -3,25 +3,14 @@ from pathlib import Path
 from colorama import Fore, Style
 
 from psd_tools import PSDImage
-from helpers import input_directory, directory_exists, psd_name
+from helpers import valid_directory, directory_exists, psd_name, decimal_count, underscore, convert_width_if_email, input_message
 
 """
     Exports images from a photoshop file
     
 """
 
-def save_image(image, size, count):
-    if len(count) <= 9:
-        image.convert('RGB').save(
-            Path(user_directory).joinpath('images', f'{name_pattern}_{size}_0{str(len(count))}.jpg'), quality=85)
-
-    if len(count) > 9:
-        image.convert('RGB').save(
-            Path(user_directory).joinpath('images', f'{name_pattern}_{size}_{str(len(count))}.jpg'), quality=85)
-
-
-def filter_layers(artboard, count):
-    size = artboard.width
+def filter_layers(artboard: 'psd_tools.api.layers.Artboard', count: list, pattern: str):
     try:
         for layer in reversed(list(artboard.descendants())):
             if 'image'.lower() in layer.name.lower() \
@@ -30,30 +19,37 @@ def filter_layers(artboard, count):
                 try:
                     count.append(layer)
                     image = layer.compose()
-                    save_image(image, size, count)
-
+                    save_image(image, size, len(count), pattern)
                 except AttributeError:
                     pass
     except AttributeError:
         pass
 
-if __name__ == "__main__":
-    user_directory = input_directory(message='File path:')
+def save_image(image: 'PIL.Image.Image', size: str, count: int, pattern: str):
+    count = decimal_count(count)
+    image.convert('RGB').save(
+        Path(user_directory).joinpath('images', f'{pattern}{size}_{count}.jpg'), quality=85)
 
-    psd_name = psd_name(user_directory, message='PSD name (can be blank):')
+
+if __name__ == "__main__":
+    user_directory = valid_directory(message='File path:')
+
+    psd_name = psd_name(user_directory, message='PSD name (can be blank or without file extension):')
 
     path_of_psd = Path(user_directory).joinpath(psd_name)
 
-    name_pattern = input('Naming convention (2019-01-01_SS19_C1_GG_Gender): \n')
+    user_input = input('Naming convention (2019-01-01_SS19_C1_GG_Gender): \n')
+    naming_convention = underscore(user_input)
 
     print(f'\nLoading {psd_name}')
     psd_load = PSDImage.open(path_of_psd)
     print(f'Finished loading {psd_name}\n')
 
     os.makedirs(Path(user_directory).joinpath('images'), exist_ok=True)
-
+    
     print('Exporting images...')
     for layer in reversed(list(psd_load)):
         count=[]
-        filter_layers(layer, count)
+        size = convert_width_if_email(str(layer.width))
+        filter_layers(layer, count, naming_convention)
     print('Done.')
