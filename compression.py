41e2379f267e2d4bdb2d1fc632ebd247c06511e3
constuelo <ferguson.rory@gmail.com>
2019-07-24 11:37:07 +0100

    Add compression and config setup

--- compression.py
+++ compression.py
@@ -1,25 +1,20 @@
+import sys
 import os
 import requests
 import logging
-from tqdm import tqdm
 from pathlib import Path
 
 logging.captureWarnings(True)
 
-if __name__ == "__main__":
-    import config
-else:
-    import config as config
-
 
 class ImageOptimCompression(object):
 
-    def __init__(self):
+    def __init__(self, username, path):
         self.endpoint = 'https://im2.io'
-        self.username = config.username
+        self.username = username
         self.quality = 'full'
-        self.path = ''
-        self.output = ''
+
+        self.path = Path(path)
         self.url = self.build_url()
 
     def build_url(self):
@@ -29,16 +24,28 @@ class ImageOptimCompression(object):
             self.quality
         ]
         return '/'.join(url_parts)
+    
+    def _image(self, name):
+        path = Path(self.path).joinpath(name)
+        if os.path.exists(path):
+            self.image_path = path
+        else:
+            sys.exit(f'{name} not found in {self.path}')
 
-    def request(self, path, name):
-        self.path = Path(path)
-        self.output = self.path.joinpath('images')
+    def request(self, name):
+        self._image(name)
+        print(name)
+        files = {'upload_file': open(self.image_path, 'rb')}
 
-        image = self.output.joinpath(name)
-        files = {'upload_file': open(image, 'rb')}
-        
-        r = requests.post(str(self.url), files=files, stream=True, verify=False)
-
-        with open(image, 'wb') as fd:
-            for chunk in r.iter_content(chunk_size=1048):
+        try:
+            self.r = requests.post(str(self.url), files=files, stream=True, verify=False)
+            self.r.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            print(e)
+            sys.exit(1)
+        self._save()
+    
+    def _save(self):
+        with open(self.image_path, 'wb') as fd:
+            for chunk in self.r.iter_content(chunk_size=1048):
                 fd.write(chunk)
--- image_export.py
+++ image_export.py
@@ -7,6 +7,8 @@ from helpers import *
 
 from psd_tools import PSDImage
 
+root = os.path.dirname(os.path.abspath(__file__))
+
 """
     Exports images from a photoshop file
     
@@ -18,7 +20,7 @@ def new_psd(path: str, layer: 'psd_tools.api.smart_object.SmartObject') -> 'PIL.
     return PSDImage.open(file_psd).compose()
 
 
-def filter_layers(artboard: 'psd_tools.api.layers.Artboard', count: list, pattern: str, user_directory: str, size: str ):
+def filter_layers(artboard: 'psd_tools.api.layers.Artboard', count: list, pattern: str, user_directory: str, size: str, output: 'pathlib.WindowsPath'):
     try:
         for layer in reversed(list(artboard)):
             if layer.kind == 'group':
@@ -33,9 +35,9 @@ def filter_layers(artboard: 'psd_tools.api.layers.Artboard', count: list, patter
                                 count.append(i)
                                 layer = i.smart_object
                                 image = new_psd(user_directory, layer)
-                                name = save_image(image, size, len(count), pattern, user_directory)
+                                name = save_image(image, size, len(count), pattern, user_directory, output)
                                 remove_file(user_directory, layer.filename)
-                                compress.request(user_directory, name)
+                                compress.request(name)
                     except AttributeError:
                         pass
                 else:
@@ -45,21 +47,21 @@ def filter_layers(artboard: 'psd_tools.api.layers.Artboard', count: list, patter
                                 and i.visible:
                             count.append(i)
                             image = i.compose()
-                            name = save_image(image, size, len(count), pattern, user_directory)
-                            compress.request(user_directory, name)
+                            name = save_image(image, size, len(count), pattern, user_directory, output)
+                            compress.request(name)
             
-            filter_layers(layer, count, naming_convention, user_directory, size)
+            filter_layers(layer, count, naming_convention, user_directory, size, output)
 
-    except AttributeError:
-        pass
+    except AttributeError as e:
+        print(e)
     except TypeError:
         pass
 
-def save_image(image: 'PIL.Image.Image', size: str, count: int, pattern: str, user_directory: str):
+def save_image(image: 'PIL.Image.Image', size: str, count: int, pattern: str, user_directory: str, output):
     count = decimal_count(count)
     name = f'{pattern}{size}_{count}.jpg'
     image.convert('RGB').save(
-        Path(user_directory).joinpath('images', f'{pattern}{size}_{count}.jpg'), quality=85)
+        Path(output).joinpath(f'{pattern}{size}_{count}.jpg'), quality=85)
     return name
 
 
@@ -73,16 +75,33 @@ if __name__ == "__main__":
     user_input = input('Naming convention (2019-01-01_SS19_C1_GG_Gender): \n')
     naming_convention = underscore(user_input)
 
+    output = Path(user_directory).joinpath('images')
+    os.makedirs(output, exist_ok=True)
+    
+    try:
+        if __name__ == "__main__":
+            import config
+        else:
+            import config as config
+    except:
+        print('First time installation requires config.py file.')
+        print('Creating config.py...')
+        config_file = open(Path(root).joinpath('config.py'), 'w')
+
+        username = input('Whats your ImageOptim API username?')
+        config_file.write(f"username='{username}'")
+        config_file.close()
+
+        import config
+
+    compress = ImageOptimCompression(username=config.username, path=output)
+
     print(f'\nLoading {psd_name}')
     psd_load = PSDImage.open(path_of_psd)
-
-    os.makedirs(Path(user_directory).joinpath('images'), exist_ok=True)
-    
-    compress = ImageOptimCompression()
 
     print('\nExporting images...')
     for layer in reversed(list(psd_load)):
         count=[]
         size = convert_width_if_email(str(layer.width))
-        filter_layers(layer, count, naming_convention, user_directory, size)
+        filter_layers(layer, count, naming_convention, user_directory, size, output)
     print('Done.')
